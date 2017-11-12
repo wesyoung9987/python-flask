b4b0d183e542f011225d4e4fe51a53aeb1055518
Wes Young <wes.young14@gmail.com>
2017-11-12 08:14:08 -0700

    Fixed not importing db when running the app and updated PORT variable

--- app.py
+++ app.py
@@ -13,10 +13,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'secretkey'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 jwt = JWT(app, authenticate, identity) # /auth
 
 api.add_resource(Store, '/store/<string:name>')
@@ -28,5 +24,4 @@ api.add_resource(UserRegister, '/register')
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(port=5000, debug=True)
