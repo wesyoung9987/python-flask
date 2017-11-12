f86c72f0076335f0c60e843f07b3bb9b6f5cc8b6
Wes Young <wes.young14@gmail.com>
2017-11-12 08:01:27 -0700

    updated port in app

--- app.py
+++ app.py
@@ -28,4 +28,5 @@ api.add_resource(UserRegister, '/register')
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
