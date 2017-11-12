7c521c8327f3b37c0fc6631572f92d75c6a35aab
Wes Young <wes.young14@gmail.com>
2017-11-12 09:47:53 -0700

    Increased data limit for password hash

--- models/user.py
+++ models/user.py
@@ -7,7 +7,7 @@ class UserModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80))
-    pw_hash = db.Column(db.String(80))
+    pw_hash = db.Column(db.String(120))
 
     def __init__(self, username, password):
         self.username = username
